82d0ac65b9cf36b4cac4de76adaa3a67161e65e6
Leonardo Barbosa <lsbarbosa@gmail.com>
2016-08-18 16:15:23 -0500

    and now it has my files...

--- python/virtualenvwrapper.zsh
+++
@@ -1,6 +0,0 @@
-# virtualenvwrapper configuation:
-export VIRTUALENVWRAPPER_PYTHON="/usr/local/bin/python"
-export WORKON_HOME="$HOME/.virtualenvs"
-export PROJECT_HOME=$CODE
-source "/usr/local/bin/virtualenvwrapper.sh"
-export VIRTUAL_ENV_DISABLE_PROMPT=1
